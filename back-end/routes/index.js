15552f8eb85c289e6f8363ae25ce107417c40df4
falahyan <falahyaniyan@gmail.com>
2024-02-02 23:03:45 +0700

    Feat: auth routes
    authentication tokenization using username and password

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+const authRoutes = require('./routes.auth');
 const userRoutes = require('./route.users');
 
+router.use(authRoutes);
 router.use(userRoutes);
 
 module.exports = router;

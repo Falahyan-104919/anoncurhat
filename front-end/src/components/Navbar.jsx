810f7dd50294caf6cf9fe39f4a9b8919f74dddde
falahyan <falahyaniyan@gmail.com>
2024-02-15 19:06:25 +0700

    Feat: User Dropdown Menu
    replace logout button with user dropdown menu and attach logout hooks to logout menu button

--- front-end/src/components/Navbar.jsx
+++ front-end/src/components/Navbar.jsx
@@ -1,13 +1,11 @@
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { Button } from './ui/button';
 import LoginDialog from '@/features/authentication/components/LoginDialog';
-import { logout } from '@/features/authentication/hooks/authSlice';
-import { LogOut } from 'lucide-react';
+import UserDropdownMenu from './DropdownMenu';
 
 export default function Navbar() {
   const authState = useSelector((state) => state.auth);
   const { isLoggedIn } = authState;
-  const dispatch = useDispatch();
   return (
     <div className="flex flex-row items-center justify-between h-16 bg-zinc-950">
       <h1 className="scroll-m-20 text-3xl font-extrabold text-white tracking-tight pl-4 lg:text-3xl">
@@ -15,10 +13,7 @@ export default function Navbar() {
       </h1>
       {isLoggedIn ? (
         <div className="flex flex-row items-center justify-between gap-4 pr-4">
-          <Button variant="destructive" onClick={() => dispatch(logout())}>
-            <LogOut size="24px" style={{ marginRight: '6px' }} />
-            Log Out
-          </Button>
+          <UserDropdownMenu />
         </div>
       ) : (
         <div className="flex flex-row items-center justify-between gap-4 pr-4">

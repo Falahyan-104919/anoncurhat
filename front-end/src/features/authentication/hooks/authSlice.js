ffc5b35c466e41ce04ef522e71aa8ce6bb10e52f
falahyan <falahyaniyan@gmail.com>
2024-02-15 17:39:58 +0700

    Fix: auth error handling

--- front-end/src/features/authentication/hooks/authSlice.js
+++ front-end/src/features/authentication/hooks/authSlice.js
@@ -3,8 +3,12 @@ import axiosInstance from '@/utils/axios';
 import { calculateAge } from '@/utils/helper';
 
 export const login = createAsyncThunk('auth/login', async (credentials) => {
-  const res = await axiosInstance.post('/auth/login', credentials);
-  return res.data;
+  try {
+    const res = await axiosInstance.post('/auth/login', credentials);
+    return res.data;
+  } catch (err) {
+    throw err;
+  }
 });
 
 const initialState = {
@@ -33,18 +37,30 @@ export const authSlice = createSlice({
     },
   },
   extraReducers: (builder) => {
-    builder.addCase(login.fulfilled, (state, action) => {
-      const { token, user_data } = action.payload;
-      state.isLoggedIn = true;
-      state.user_id = user_data['id_user'];
-      state.username = user_data['username'];
-      state.gender = user_data['gender'];
-      state.age = calculateAge(user_data['date_of_birth']);
-      state.role = user_data['role'];
-      axiosInstance.defaults.headers.common['Authorization'] = token;
-      const serializedState = JSON.stringify(state);
-      localStorage.setItem('authState', serializedState);
-    });
+    builder
+      .addCase(login.fulfilled, (state, action) => {
+        const { token, user_data } = action.payload;
+        state.isLoggedIn = true;
+        state.user_id = user_data['id_user'];
+        state.username = user_data['username'];
+        state.gender = user_data['gender'];
+        state.age = calculateAge(user_data['date_of_birth']);
+        state.role = user_data['role'];
+        axiosInstance.defaults.headers.common['Authorization'] = token;
+        const serializedState = JSON.stringify(state);
+        localStorage.setItem('authState', serializedState);
+      })
+      .addCase(login.rejected, (state, action) => {
+        state.error = action.error.message || 'An unknown error occurred';
+        state.isLoggedIn = false;
+        delete state.user_id;
+        delete state.username;
+        delete state.gender;
+        delete state.age;
+        delete state.role;
+        delete axiosInstance.defaults.headers.common['Authorization'];
+        localStorage.removeItem('authState');
+      });
   },
 });
 

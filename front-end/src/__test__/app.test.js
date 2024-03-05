1e5faecd07bbcecf36704714f0be7436ad102510
Sandra Marsella <sandra03marsella@gmail.com>
2024-03-06 02:50:39 +0700

    update: testing jest

--- front-end/src/__test__/app.test.js
+++ front-end/src/__test__/app.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import { BrowserRouter as Router } from "react-router-dom";
-import App from "../path/to/App";
+import App from "@/App";
 
 test("renders App component", () => {
   render(
--- front-end/src/__test__/jest.config.js
+++ front-end/src/__test__/jest.config.js
@@ -2,3 +2,9 @@ module.exports = {
   testEnvironment: "jsdom",
   setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
 };
+
+module.exports = {
+  transform: {
+    "^.+\\.(js|jsx)$": "babel-jest",
+  },
+};
